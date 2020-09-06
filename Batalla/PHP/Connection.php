17842a4149fd4ae885470242944875c1aaa25466
Francisco Javier Rodriguez Hernandez <FJ_RH@hotmail.com>
2020-09-06 14:36:46 -0400

    Cambio en el sistema de conexion

--- Batalla/PHP/Connection.php
+++ Batalla/PHP/Connection.php
@@ -1,15 +1,37 @@
 <?php 
-
-function getConnection(){
-    $DB_Adress = "localhost";
-    $DB_User = "Javier";
-    $DB_Password = "pi3coma14";
-    $DB_Name = "batallaprealfa_0_1";   
-    $Connection = new mysqli($DB_Adress, $DB_User, $DB_Password, $DB_Name);
-    if($Connection->connect_error){
-        die("Error al acceder a la Base de Datos");
+    function getConnection(){
+        try {
+            $DBHost = "localhost";
+            $DBUser = "Javier";
+            $DBPassword = "pi3coma14";
+            $DBName = "batallaprealfa_0_1";
+            $Connection = new PDO("mysql:host=$DBHost;dbname=$DBName", $DBUser, $DBPassword);
+            $Connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $Connection->exec("SET CHARACTER SET utf8");
+            return $Connection;
+        } catch (Exception $error) {
+            die("Error al intentar conectar con el servidor");   
+        }
     }
-    return $Connection;
-}
 
+    function getUser($User, $Pass){
+        $User = htmlentities(addslashes($User));
+        $Pass = htmlentities(addslashes($Pass));
+        $Connection = getConnection();
+        $SQL= "SELECT * FROM users WHERE usuario= :Usuario AND pass= :Pass";
+        try {
+            $ResultSet = $Connection->prepare($SQL);
+            $ResultSet->bindValue(":Usuario", $User);
+            $ResultSet->bindValue(":Pass", $Pass);
+            $ResultSet->execute();
+            if($ResultSet->rowCount() != 0){
+                return json_encode($ResultSet->fetch(PDO::FETCH_ASSOC));
+            }
+            else{
+                return "No-Econtrado";
+            }
+        } catch (Exception $error) {
+            die("Error al realizar la consulta");
+        }
+    }
 ?>
--- Batalla/PHP/ValidarLogin.php
+++ Batalla/PHP/ValidarLogin.php
@@ -4,56 +4,38 @@ require "Connection.php";
 
 $User = $_POST["User"];
 $Pass = $_POST["Password"];
-
 $Expresion = array(
     "Usuario" => "/^[a-zA-Z0-9\_\-]{4,16}$/",
     "Password" => "/^.{4,12}$/"
 );
 
-//Si no tiene usuario y password se redirecciona al login
 if(!isset($_POST["User"]) || !isset($_POST["Password"])){
 
     header("Location: ../Index.html");
 }
-else if(!preg_match($Expresion["Usuario"], $User) || !preg_match($Expresion["Password"], $Pass)){ //Si los datos no cumplen los requerimientos se redirecciona al login
+else if(!preg_match($Expresion["Usuario"], $User) || !preg_match($Expresion["Password"], $Pass)){ 
 
     header("Location: ../Index.html");
 }
-else{ //si todo lo demas es falso, hace la consulta en la base de datos
+else{ 
 
- 
-    $BD_Query = "SELECT * FROM users WHERE usuario = '$User' AND pass = '$Pass'";
-    
-    $Connection = getConnection();
+    $Respuesta = getUser($User, $Pass);
 
-    $ResultSet = $Connection->query($BD_Query);
-
-    if(!$Row = $ResultSet->fetch_assoc()){
-        echo "Usuario o Contraseña invalidos";
-    }
-    else{
-        //Variables de session
-        $_SESSION["Name"] = $Row["Nombre"];
-        $_SESSION["User"] = $Row["usuario"];
-        $_SESSION["Password"] = $Row["pass"];
-
-        $_SESSION["SERVIDOR"] = basename($_SERVER['PHP_SELF']); //y crea una variable que contiene la direccion del server
-        
+    if(!strcmp($Respuesta, "No-Econtrado" )){
+        echo "Usuario o Contraseña inconectos";
+    }else{
+        $_SESSION["SERVIDOR"] = basename($_SERVER['PHP_SELF']);
+        $Respuesta = json_decode($Respuesta, true);
+        $_SESSION["Name"] = $Respuesta["Nombre"];
+        $_SESSION["User"] = $Respuesta["usuario"];
+        $_SESSION["Password"] = $Respuesta["pass"];
         $_SESSION["Secciones"] = array();
-
         for ($i = 1 ; $i <=10 ; $i++){
-            if($Row["Mat" . $i] != ""){
-                $_SESSION["Secciones"] = $Row["Mat" . $i];
+            if($Respuesta["Mat$i"] != ""){
+            $_SESSION["Secciones"] = $Respuesta["Mat$i"];
             }
         }
-     
         echo "ProfesorEcontrado";
-       
     }
-
 }
-
-
-
-
 ?>
